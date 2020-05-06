bee6d8469f3275219654de2c8b95ee78ed0f4974
Андрей <andrey@MacBook-Air-Admin.local>
2020-05-06 13:08:05 +0300

    update src/cli.js забыл return

--- src/cli.js
+++ src/cli.js
@@ -5,6 +5,6 @@ const greeting = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   const greetingName = (`Hello, ${name}!`);
-  console.log(greetingName);
+  return greetingName;
 };
 export default greeting;
