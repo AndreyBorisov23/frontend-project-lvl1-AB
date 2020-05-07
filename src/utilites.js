4a7a1c671e8f4d876e2acf47274e87624ff5c390
Андрей <andrey@MacBook-Air-Admin.local>
2020-05-07 13:48:02 +0300

    fixed errors in utilites.js

--- src/utilites.js
+++ src/utilites.js
@@ -12,8 +12,9 @@ const miniCalc = (num1, separator, num2) => {
     return num1 + num2;
   } else if (separator === '-') {
     return num1 - num2;
+  } else {
+    return num1 * num2;
   }
-  return num1 * num2;
 };
 
 const getDivisor = (value1, value2) => (
@@ -29,17 +30,17 @@ const progression = (start, length, index) => {
   return result;
 };
 
-const isPrime = (num) => {
-    if (num < 2) {
-      return 'no';
-    }
-    for (let i = 2; i < num; i += 1) {
-      if (num % i === 0) {
-        return 'no';
-      }
-    }
-    return 'yes';
-  };
+const isPrime = (num) => {
+  if (num < 2) {
+    return 'no';
+  }
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) {
+      return 'no';
+    }
+  }
+  return 'yes';
+};
 
 export {
   generateRandomNumber,
